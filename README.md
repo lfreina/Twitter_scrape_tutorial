aa2ea20a460350e3a0ae4b6ba0ebda03fd7482bf
Lucho <luis.fernando.reina@advectas.se>
2019-01-17 14:54:45 +0100

    Better documentation

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
 # Twitter_scrape_tutorial
+Scrape Twitter for a search word and get 5 similar words and the 5 most frequent words around the search word.
+Built in Python3.x
 
+## Setup
+To install modules, you need the python package manager pip (sudo apt-get install pip).
+
+### Required files
 You will need the files from The Swedish Word Vectors from 10M blog posts from Mattias Östmar.
-
 You can find the files at: https://osf.io/y37g2/
 
-# This web app is going to go through 4 main steps:
-1.- Scrape twitter for keywords ( comma separated input )
-2.- Get an embedding for every tweet using word2vec as a test tool. 
-3.- Perform PCA to the vectors for every tweet.
-4.- Plot the results using plotly.
+### Virtual environment
+It is recommended to use a virtual environment to install packages in, so they don't get installed globally.
+There are many tools for this but the most used low-lever is called virtualenv.
+
+> pip install virtualenv
+
+Create a virtual environment for a project:
+
+> cd my_project_folder && virtualenv env -p python3.6
+
+This creates a copy of Python3.6 in whichever directory you ran the command in, placing it in a folder named 'env'.
+To begin using the virtual environment, it needs to be activated:
+
+> source env/bin/activate
+
+This will now show in the terminal prompt. To deactivate the venv, just use the command
+
+> deactivate
+
+In Pycharm, you can also choose the virtualenv to be the standard python interpreter.
+
+### Installation
+The easiest way to install the module and the required packages is to use pip.
+The following will install the required python packages.
+Don't forget to activate your virtual environment first!
+
+>pip install twitterscraper gensim nltk
+
--- scrape.py
+++ scrape.py
@@ -3,6 +3,10 @@
 # TODO:
 # get just one word or verify agains more than one the search.
 
+# Got the stopwords from: https://github.com/stopwords-iso/stopwords-sv/blob/master/stopwords-sv.txt
+# To use run:
+#    pip install twitterscraper gensim nltk
+
 from twitterscraper import query_tweets
 import gzip
 import gensim
@@ -11,57 +15,88 @@ import os
 import codecs
 import numpy as np
 from nltk import FreqDist
+import datetime as dt
+import sys
+
+def test_bib (word, stopwords):
+    '''
+    Function that will scrape Twitter for the "word".
+
+    word: One word to scrape twitter for
+    stopwords: A list with words that are to be considered stopwords
+               from the scraped text.
 
 
-def test_bib (orden, stopwords):
-        all_text=[]
-        words = []
+    TODO: Verify that the stopwords are actually removed from the scraped text.
+    '''
+    all_text=[]
+    words = []
+    # Peform the query in swedish
+    # poolsize will be used for querying the dates as follows:
+    #     dateranges = begindate + linspace(0, #days, poolsize+1 )
+    # You can set a limit of tweets per pool, but why?
+    
+    start_date = dt.date(2008,1,1)
+    stop_date = dt.date.today()
+    for query in query_tweets(word,lang='sv',poolsize=10,begindate=start_date,enddate=stop_date):
+        # 1. Change to lower case
+        new_list = query.text.lower().split(u' ')
+        # 1.5 TODO: Remove https/http links
 
-	for query in query_tweets(orden, lang='sv',poolsize=100):
-                new_list = query.text.split(u' ')
+        new_sentence = []
+        # 2. Remove the stopwords in a very naive way
+        for word in new_list:
+            if word not in stopwords:
+                new_sentence.append(word)
+                words.append(word)
                 
-                for word in new_list:
-                        try:
-                                if word.encode('utf-8').lower() in stopwords:
-                                        new_list.remove(word)#.encode('utf-8'))
-                                else:
-                                        words.append(word.lower())
-                        except:
-                                print word, " is not unicode"
-                new_sentence = []
-                for word in new_list:
-                        new_sentence.append(word.lower())
-                all_text.append(new_sentence)
-        return all_text, words
+        all_text.append(new_sentence)
+    # Return the list with sentences and a list with words
+    unique_words = np.unique(words)
+    return all_text, unique_words
 
-# Got the stopwords from: https://github.com/stopwords-iso/stopwords-sv/blob/master/stopwords-sv.txt
-stopwords_sv = []
-with open('stopwords-sv.txt','r') as sw:
-        for line in sw:
-                word_to_insert = line.replace('\n','')                
-                stopwords_sv.append(word_to_insert)#.encode('utf-8'))
 
 # Print the similar words given a model
 def get_similar(word_to_search,model):
-        print "The closest words to ", word_to_search
-        for word,freq in model.wv.most_similar(positive=word_to_search.lower(),
-                                               topn=5):
-                print word
+    '''
+    word_to_search: Print similar words to word_to_search with a model.
+    model: Is a word2vec model
+    '''
+    print ("The closest words to ", word_to_search )
+    for word,freq in model.wv.most_similar(positive=word_to_search.lower(),
+                                                topn=5):
+        print ( word )
+        
 # Print the 5 most common given a freq analysis result
 def get_most_freq(word_to_search, freq_analysis):
-        print "The most frequent words around ", word_to_search.lower()
-        counter = 0
-        for word,freq in freq_analysis.most_common(20):
-                if counter == 5:
-                        break
-                if word_to_search.lower() in word:
-                        pass
-                else:
-                        print "The word: ", word, ", |  Times:", freq
-                        counter+=1
+    '''
+    word_to_search: 5 most frequent words around this word
+    freq_analysis: an nltk FreqDist result
+    '''
+    print ("The most frequent words around ", word_to_search.lower() )
+    counter = 0
+    for word,freq in freq_analysis.most_common(20):
+        if counter == 5:
+            break
+        if word_to_search.lower() in word:
+            pass
+        else:
+            print ("The word: ", word, ", |  Times:", freq )
+            counter+=1
                 
 
 # Main
+# --------------------------------
+# Create a stopwords list
+# --------------------------------
+stopwords_sv = []
+# Remove the new lines in the file
+with open('stopwords-sv.txt','r') as sw:
+    for line in sw:
+        word_to_insert = line.replace('\n','')                
+        stopwords_sv.append(word_to_insert)
+
+
 
 # Add some stop words to the list.
 stopwords_sv.append(u'-')
@@ -70,23 +105,28 @@ stopwords_sv.append(u'')
 stopwords_sv.append(u'.')
 stopwords_sv.append(u'&')
 
-# Define the words/word to search
-words_to_search = "Advectas"
+# --------------------------------
+# Define the word to search
+# --------------------------------
+words_to_search = "hej"
 documents, words = test_bib(words_to_search,stopwords_sv)
 
-
-# build vocabulary and train model
-model = gensim.models.Word2Vec(
-        documents,
-        size=150,
-        window=20,
-        min_count=2,
-        workers=4,
-        sg=0)
+if len(words) == 0:
+    sys.exit('No tweets fetched :S !! ')
+# --------------------------------
+# Maybe remove this? I do not think we need it!
+# --------------------------------
+# build vocabulary and train Word2Vec model with the scraped text
+model = gensim.models.Word2Vec(documents,
+                               size=150,
+                               window=20,
+                               min_count=2,
+                               workers=4,
+                               sg=0)
 
 model.train(documents, total_examples=len(documents), epochs=10)
 get_similar(words_to_search,model)
-print "------------------"
+print ("------------------")
 freq_analysis = FreqDist(words)
 get_most_freq(words_to_search, freq_analysis)
 
